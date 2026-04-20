091b82344151b2e85c4f81d91be4e6a8acf17288
Seakorv <arttudun@gmail.com>
2026-04-21 00:20:10 +0300

    Made basic front with react and back with node
    Started the frontend with react and backend with node. API calls work,
    front demonstrates it simply with the licence plate of the first vehicle
    of VehiclePositions.
    
    Everything runs only locally still, start front in jkl-transport-front
    directory with npm run dev
    
    Start back end in jkl-transport-back with npm start
    
    API data in json can be found in
    http://localhost:3001/api/tripupdate/json or /vehicleposition/json

---
+++ jkl-transport-back/index.js
@@ -0,0 +1,114 @@
+require('dotenv').config()
+
+const axios = require('axios')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const GtfsRealtimeBindings = require('gtfs-realtime-bindings')
+
+app.use(cors())
+
+//.env file with your API keys from Waltti
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+
+const auth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64")
+
+app.get("/api/tripupdate", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://data.waltti.fi/jyvaskyla/api/gtfsrealtime/v1.0/feed/tripupdate",
+      {
+        responseType: "arraybuffer",
+        headers: {
+          Authorization: `Basic ${auth}`,
+        },
+      }
+    )
+
+    res.send(response.data)
+  } catch (err) {
+    console.error(err)
+    if (!response?.data) {
+      res.status(500).json({ error: "Failed to fetch GTFS data"})
+    }
+  }
+})
+
+//JSON is nicer to read
+app.get('/api/tripupdate/json', async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://data.waltti.fi/jyvaskyla/api/gtfsrealtime/v1.0/feed/tripupdate",
+      {
+        responseType: 'arraybuffer',
+        headers: {
+          Authorization: `Basic ${auth}`,
+        },
+      }
+    )
+    //same as in front end
+    const feed =
+      GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+        new Uint8Array(response.data)
+      )
+
+    res.json(feed)
+
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Failed to fetch GTFS data'})
+  }
+})
+
+app.get("/api/vehicleposition", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://data.waltti.fi/jyvaskyla/api/gtfsrealtime/v1.0/feed/vehicleposition",
+      {
+        responseType: "arraybuffer",
+        headers: {
+          Authorization: `Basic ${auth}`,
+        },
+      }
+    )
+
+    res.send(response.data)
+  } catch (err) {
+    console.error(err)
+    if (!response?.data) {
+      res.status(500).json({ error: "Failed to fetch GTFS data"})
+    }
+  }
+})
+
+//JSON is nicer to read
+app.get('/api/vehicleposition/json', async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://data.waltti.fi/jyvaskyla/api/gtfsrealtime/v1.0/feed/vehicleposition",
+      {
+        responseType: 'arraybuffer',
+        headers: {
+          Authorization: `Basic ${auth}`,
+        },
+      }
+    )
+    //same as in front end
+    const feed =
+      GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+        new Uint8Array(response.data)
+      )
+
+    res.json(feed)
+
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Failed to fetch GTFS data'})
+  }
+})
+
+const PORT = process.env.PORT||3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
